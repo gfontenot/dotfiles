4c0486954d12f4e4f052341e8f52c49825a237f3
Gordon Fontenot <gordon.fontenot@gmail.com>
2011-01-11 14:57:46 -0500

    more tweaks for linux

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -9,4 +9,21 @@ then
   alias la='gls -A --color'
 fi
 
+if [ "$(uname)" = "Linux" ]
+then
+  if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls -F --color=auto'
+    alias l='ls -lAh --color=auto'
+    alias ll='ls -l --color=auto'
+    alias la='ls -A --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+  fi 
+fi
+
 mcd () {mkdir -p "$@" && cd "$@"; }
