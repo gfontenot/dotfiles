a5c717341182993c2737c500b3c22bfff07cd3c3
Gordon Fontenot <gordon.fontenot@me.com>
2011-06-29 21:41:50 -0400

    harumpfh

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,17 +3,6 @@
 
 alias git=hub
 
-# I use JRuby substantially, and we want to make sure hub is run using MRI
-# regardless of which Ruby you're using or else the `git status` in your prompt
-# will take seven thousand seconds to run `ls`.
-#
-# I'm hardcoding it to an installed rvm (using rvm's `rvm 1.8.7,ruby /hub/path`
-# syntax is way too slow). It should work fine for those without rvm, though.
-if [[ -s $HOME/.rvm/scripts/rvm ]] 
-then
-  alias git='$HOME/.rvm/rubies/ruby-1.8.7-p302/bin/ruby `which hub`'
-fi
-
 # The rest of my fun git aliases
 alias gl='git pull --rebase --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
