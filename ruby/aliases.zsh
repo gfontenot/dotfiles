4ac5c8511f0aaa770c296650aa56a11943b99bc6
Gordon Fontenot <gordon.fontenot@me.com>
2011-08-01 11:56:57 -0400

    bweght

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,12 +1,3 @@
-alias r='rvm use 1.8.7'
-
-alias f='RAILS_ENV=fi'
-
-alias sc='script/console'
-alias ss='script/server -p `available_rails_port`'
-alias sg='script/generate'
-alias sd='script/destroy'
-
 alias migrate='rake db:migrate db:test:clone'
 
-alias s="ps aux | grep \"[r]uby\" | grep script/server || echo \"You're not running any, dawg.\""
+alias s="ps aux | grep \"ruby\" | grep "script/rails s" || echo \"You're not running any, dawg.\""
