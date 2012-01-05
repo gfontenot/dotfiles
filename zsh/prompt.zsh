a2eed985b530818175842a08f68c7ac67b32fe4d
Gordon Fontenot <gordon.fontenot@me.com>
2012-01-05 17:23:29 -0500

    Tweak my prompt, and add git_cdw_info to help

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -32,44 +32,12 @@ need_push () {
   then
     echo " "
   else
-    echo "%{\e[0m%}with %{\e[1;35m%}unpushed"
+    echo " %{\e[31m%}unpushed"
   fi
-}
-
-rvm_prompt(){
-  if $(which rvm &> /dev/null)
-  then
-	  echo "%{\e[0;33m%}$(rvm tools identifier)"
-	else
-	  echo ""
-  fi
-}
-
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if $(which todo.sh &> /dev/null)
-  then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
-    fi
-  else
-    echo ""
-  fi
-}
-
-directory_name(){
-  echo "%{\e[0;36m%}%1/"
 }
 
 project_pwd() {
-  echo $PWD | sed -e "s/\/Users\/$USER/~/" -e "s/~\/projects\/\([^\/]*\)\/current/\\1/" -e "s/~\/Sites\/\([^\/]*\)\/current/http:\/\/\\1/"
+  echo $PWD | sed -e "s/\/Users\/$USER/~/" -e "s/~\/Dropbox\/Personal\/!!Code\/\([^\/]*\)/\\1/"
 }
 
 ruby_version() {
@@ -79,4 +47,4 @@ ruby_version() {
 export PROMPT=$'%{\e[0;90m%}%n@%m%{\e[0m%}
 %{\e[0;%(?.32.31)m%}>%{\e[0m%} '
 
-export RPROMPT=$'$(directory_name) $(git_dirty) $(need_push)%{\e[0m%}'
+export RPROMPT=$'$(project_pwd)$($ZSH/bin/git_cwd_info)$(need_push)%{\e[0m%}'
