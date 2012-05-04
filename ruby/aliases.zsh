99c93834eee48f0ced47e06ee60cfe4cd070651f
Gordon Fontenot <gordon.fontenot@me.com>
2012-05-04 13:36:38 -0400

    Add an alias to the pretty version of rspec

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,6 @@
 alias migrate='rake db:migrate db:test:clone'
 
 alias s="ps aux | grep \"ruby\" | grep "script/rails s" || echo \"You're not running any, dawg.\""
+
+# alias the pretty version of rspec
+alias tst='rspec --color --format doc'
