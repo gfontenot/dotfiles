73827d6e84b8674e6455b8f545b643597bc8c04b
Gordon Fontenot <gordon.fontenot@me.com>
2012-05-24 17:02:31 -0400

    remove unused s alias

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,6 +1,4 @@
 alias migrate='rake db:migrate db:test:clone'
 
-alias s="ps aux | grep \"ruby\" | grep "script/rails s" || echo \"You're not running any, dawg.\""
-
 # alias the pretty version of rspec
 alias tst='rspec --color --format doc'
