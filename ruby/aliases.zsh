0ecd36894ca84689253849a4fa85d1ad7b8d9d3f
Gordon Fontenot <gordon.fontenot@me.com>
2012-05-25 13:21:57 -0400

    tweakin tweaks

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -3,4 +3,4 @@ alias migrate='rake db:migrate db:test:clone'
 # alias the pretty version of rspec
 alias tst='rspec --color --format doc'
 
-alias bi="bundle install --binstubs=.bin"
+alias bi="bundle install --path vendor/bundle && bundle install --binstubs=.bin"
--- ruby/path.zsh
+++
@@ -1,4 +0,0 @@
-# Add the local .bin dir to the PATH
-# Useful for rails apps executing the binstubs
-
-export PATH="./bin:$PATH"
