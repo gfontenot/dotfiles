2d2ea32b73248bb9de21efc4da52ab5fdcb08e25
Gordon Fontenot <gordon.fontenot@me.com>
2012-05-25 12:56:59 -0400

    Add helpers for bundler
    alias a command to install binstubs
    add the local .bin dir to the path
    have git ignore the .bin dir

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -2,3 +2,5 @@ alias migrate='rake db:migrate db:test:clone'
 
 # alias the pretty version of rspec
 alias tst='rspec --color --format doc'
+
+alias bi="bundle install --binstubs=.bin"
---
+++ ruby/path.zsh
@@ -0,0 +1,4 @@
+# Add the local .bin dir to the PATH
+# Useful for rails apps executing the binstubs
+
+export PATH="./bin:$PATH"
