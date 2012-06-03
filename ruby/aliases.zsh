5ea4b9c2304885b9fcc796130e21cd5d2a308609
Gordon Fontenot <gordon.fontenot@me.com>
2012-06-03 12:00:04 -0400

    update bi alias

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -3,6 +3,6 @@ alias migrate='rake db:migrate db:test:clone'
 # alias the pretty version of rspec
 alias tst='rspec --color --format doc'
 
-alias bi="bundle install --path vendor/bundle && bundle install --binstubs=.bin"
+alias bi="bundle install --path vendor/bundle --binstubs=.bin"
 
 alias be="bundle exec"
