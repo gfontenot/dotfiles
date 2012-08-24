07a365a6dd6bc5b792b77fd2232f639413d7be1a
Gordon Fontenot <gordon.fontenot@me.com>
2012-08-24 17:14:38 -0400

    Tweakin the prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,12 +1,13 @@
+autoload colors && colors
+
 project_pwd() {
-  echo $PWD | sed -e "s/\/Users\/$USER/~/" -e "s/~\/Dropbox\/Personal\/!!Code\/\([^\/]*\)/\\1/"
+  echo $PWD | sed -e "s/\/Users\/$USER/~/" -e "s/~\/Code\/\([^\/]*\)/\\1/"
 }
 
 ruby_version() {
   echo " $(ruby -v | awk '{print $2}')"
 }
 
-export PROMPT=$'%{\e[0;90m%}%n@%m%{\e[0m%}
-%{\e[0;%(?.32.31)m%}>%{\e[0m%} '
+export PROMPT=$'%{\e[0;%(?.32.31)m%}>%{$reset_color%} '
 
-export RPROMPT=$'$(project_pwd)$($ZSH/zsh/git_cwd_info)$(ruby_version)'
+export RPROMPT=$'%c$($ZSH/zsh/git_cwd_info)$(ruby_version)'
