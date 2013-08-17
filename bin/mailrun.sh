da8dcde70b8888baf05e1d73e61062bb864a5e8a
Gordon Fontenot <gordon@thoughtbot.com>
2013-08-17 18:39:31 -0400

    Add mailrun script

---
+++ bin/mailrun.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -r pid < ~/.offlineimap/pid
+
+if ps $pid &>/dev/null; then
+  echo "offlineimap ($pid): another instance running." >&2
+  kill -9 $pid
+fi
+
+/usr/local/bin/offlineimap -o -u quiet
