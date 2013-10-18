d40a5373a1323455ed8c0d415afa9c92ee0791ec
Gordon Fontenot <gordon@thoughtbot.com>
2013-10-18 10:36:45 -0400

    Use prettier character in prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -8,6 +8,6 @@ ruby_version() {
   echo " $(ruby -v | awk '{print $2}')"
 }
 
-export PROMPT=$'%{\e[0;%(?.32.31)m%}>%{$reset_color%} '
+export PROMPT=$'%{\e[0;%(?.32.31)m%}❯%{$reset_color%} '
 
 export RPROMPT=$'%c$($ZSH/zsh/git_cwd_info)$(ruby_version)'
