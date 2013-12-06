b9783ad9f0a33987a73d28556227ce5dcc8a5eda
Gordon Fontenot <gordon@thoughtbot.com>
2013-12-06 15:09:19 -0500

    Add usr/local/bin to the path
    wtf mavericks

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,3 @@
-export PATH="$ZSH/bin:$HOME/.cabal/bin:/usr/texbin:$PATH"
+export PATH="$ZSH/bin:$HOME/.cabal/bin:/usr/texbin:/usr/local/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
