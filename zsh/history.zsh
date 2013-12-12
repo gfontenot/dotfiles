9d6de258352a6b2f91e6c8626e25681d3589947d
Gordon Fontenot <gordon@thoughtbot.com>
2013-12-12 10:40:38 -0500

    Clean history options

---
+++ zsh/history.zsh
@@ -0,0 +1,10 @@
+HISTFILE=~/.zsh_history
+HISTSIZE=10000000
+SAVEHIST=$HISTSIZE
+setopt HIST_VERIFY
+setopt SHARE_HISTORY # share history between sessions ???
+setopt EXTENDED_HISTORY # add timestamps to history
+setopt INC_APPEND_HISTORY # adds history incrementally
+setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_REDUCE_BLANKS
+
