a3b580fef92af16e91a7328d9c123da50b8a709f
Gordon Fontenot <gordon@thoughtbot.com>
2014-01-02 20:11:23 -0500

    Kill mbsync if it hung during last run
    This happens constantly. Knock it the fuck off.

--- bin/mailrun.sh
+++ bin/mailrun.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+mbsyncInstances=`ps aux | grep mbsync | wc -l`
+
+if [ $mbsyncInstances == 2 ]; then
+  echo "Another instance of mbsync is running. Killing it now."
+  killall mbsync
+fi
+
 /usr/local/bin/mbsync -q -a
