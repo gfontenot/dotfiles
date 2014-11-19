494afdc14ace4fffb47938a807cf519f6c2532fe
Gordon Fontenot <gordon@thoughtbot.com>
2014-11-19 11:26:12 -0500

    xcode: Update pre-release aliases with new name

--- tag-xcode/source/aliases.zsh
+++ tag-xcode/source/aliases.zsh
@@ -1,9 +1,9 @@
 # Open the current directory with Xcode and let it do its thang
 alias x="open -a 'Xcode' ."
-alias x-pre="open -a 'Xcode-DP' ."
+alias x-pre="open -a 'Xcode-Beta' ."
 
 alias play="open -a 'Xcode' ~/.scratchpad.playground"
-alias play-pre="open -a 'Xcode-DP' ~/.scratchpad.playground"
+alias play-pre="open -a 'Xcode-Beta' ~/.scratchpad.playground"
 
 # Clear the derived data folder cause uuuuuuggggghhhhhhh
 alias fuxcode='rm -rf ~/Library/Developer/Xcode/DerivedData/'
