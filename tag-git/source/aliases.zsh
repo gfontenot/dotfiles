17bfa193295b2f0952065424fb44eeaff8f78672
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-09-10 17:26:22 -0400

    git: add aliases for common commands
    These have historically been handled with shell aliases, but since gitsh
    wants to use git aliases instead of shell aliases, it makes sense to
    move them in here and reference them from the shell aliases.

--- tag-git/source/aliases.zsh
+++ tag-git/source/aliases.zsh
@@ -7,20 +7,20 @@ alias gdiff='g diff'
 
 # quick aliases for common actions
 alias ga='g add'
-alias gc!='g commit -v'
-alias gcm='g commit -m'
-alias gco='g checkout'
+alias gc!='g c'
+alias gcm='g cm'
+alias gco='g co'
 alias gp='g push'
 alias gmf='g merge --ff-only'
-alias gup='g update'
-alias gsup='g submodule update --init --recursive'
+alias gup='g up'
+alias gsup='g sup'
 
 alias gbc='g create-branch'
 alias gbd='g delete-branch'
 
 # log aliases
-alias gl='g log --format=custom --graph'
-alias glb='gl HEAD --not master'
+alias gl='g l'
+alias glb='g lb'
 
 # git rm deleted files
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
