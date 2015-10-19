debcf99e60d9820aac5dd37e1303d62527e60538
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-10-19 09:48:32 -0500

    git: Use long flags in aliases
    There's no reason to use the short versions of these flags. For the sake
    of readability, it's best to use the long form versions.

--- tag-git/config/git/functions.zsh
+++ tag-git/config/git/functions.zsh
@@ -1,7 +1,7 @@
 # If we aren't in a git directory, I probably wanted ls
 gs() {
   if git rev-parse --git-dir > /dev/null 2>&1; then
-    git status -sb $@
+    git status --short --branch $@
   else
     echo "This isn't a git repo. I'm going to assume you meant ls"
     ls $@
