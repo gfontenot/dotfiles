b62ff4cecb24879675696d72d5f0a515ba24bdb0
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2016-09-06 14:10:35 -0500

    git: improve completion

--- tag-git/config/git/completion.zsh
+++ tag-git/config/git/completion.zsh
@@ -1,6 +1,10 @@
+zstyle ':completion:*:*:git:*' user-commands ${${(M)${(k)commands}:#git-*}/git-/}
+
 compdef g=git
 
 compdef _git gco=git-checkout
 compdef _git gp=git-push
 compdef _git gmf=git-merge
-compdef _git gbd=git-checkout
+
+compdef _git gbd=git-branch
+_git-delete-branch() { compadd "$@" $(git branch) }
