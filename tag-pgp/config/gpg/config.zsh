f20945cb6a7a2dcce80a9def9070d822f0269c7a
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2016-09-18 19:50:35 -0500

    gpg: Upgrade to gpg 2.1
    gpg 2.1 is a breaking change (lol semver) but adds a bunch of great
    festures including not needing to manage gpg-agent manually. OK if I'm
    being honest I don't know what else gpg 2.1 introduces but fucking hell
    am I excited to not deal with that crap anymore.

--- tag-pgp/config/gpg/config.zsh
+++ tag-pgp/config/gpg/config.zsh
@@ -1,9 +1,1 @@
-export GPG_TTY=`tty`
-
-# Note that this requires gpg-agent to be running. This is controlled by a
-# launch agent script.
-if [ -f "${HOME}/.gpg-agent-info" ]; then
-  . "${HOME}/.gpg-agent-info"
-  export GPG_AGENT_INFO
-  export SSH_AUTH_SOCK
-fi
+# export GPG_TTY=`tty`
