88c9746e427f8edff1b21c4f053ff076326b3d8e
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2016-09-28 14:41:15 -0700

    zsh: Don't export CDPATH
    Whooo boy does this cause all kinds of problems. Instead, we'll export
    our own JUMP_DIRECTORIES variable that is _set_ to CDPATH. This will
    give us the same result as we used to have but without the issues.

--- tag-zsh/config/zsh/navigation.zsh
+++ tag-zsh/config/zsh/navigation.zsh
@@ -1,3 +1,6 @@
 setopt auto_cd
 cdpath=($HOME $PROJECTS $PROJECTS/thoughtbot $PROJECTS/clients $PROJECTS/gfontenot $DROPBOX)
-export CDPATH
+
+# Exporting CDPATH is bad and can cause all sorts of problems so lets set
+# CDPATH to something else and export that instead
+export JUMP_DIRECTORIES=$CDPATH
