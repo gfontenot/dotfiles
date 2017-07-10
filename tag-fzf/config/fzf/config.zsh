38e1215b0dba888903a90fc706aae9809a810846
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2017-07-10 10:48:15 -0500

    fzf: Map vim keybindings for paging
    The defaults here use readline compatible keybindings, so the actual page up
    and page down buttons handle paging. That's fine, but I'm actually not sure
    how to do page up on my laptop so lets just make this like vim.

--- tag-fzf/config/fzf/config.zsh
+++ tag-fzf/config/fzf/config.zsh
@@ -1,2 +1,6 @@
 # Use `ag` as the default command and filter out ignored files
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+
+export FZF_DEFAULT_OPTS='
+--bind ctrl-u:page-up,ctrl-f:page-down
+'
