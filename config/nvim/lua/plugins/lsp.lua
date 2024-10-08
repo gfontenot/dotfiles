910c2da6c99b5ef78fab4288d2c60cc8cb4896c3
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2024-10-08 12:33:53 -0500

    nvim: Add sourcekit-lsp
    This adds LSP support for Swift/Objective-C projects. I'm positive I'll
    need to do more tweaking here over time but this gets things up off the
    ground at least.

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -132,6 +132,17 @@ return {
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
+			-- [[ Unmanaged Servers ]]
+
+			require("lspconfig").sourcekit.setup({
+				capabilities = {
+					workspace = {
+						didChangeWatchedFiles = {
+							dynamicRegistration = true,
+						},
+					},
+				},
+			})
 
 			-- [[ Managed servers ]]
 
