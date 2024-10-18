04dadbe559177ac59d29d3b85b1ac8054f2ff29c
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2024-10-17 20:26:37 -0500

    nvim: Add vim-tmux-runner back as a dependency
    Still the best there is thank you @christoomey

---
+++ config/nvim/lua/plugins/vim-tmux-runner.lua
@@ -0,0 +1,8 @@
+return {
+	"christoomey/vim-tmux-runner",
+	keys = {
+		{ "<leader>ar", "<cmd>VtrAttachToPane<cr>" },
+		{ "<leader>fr", "<cmd>VtrFocusRunner<cr>" },
+		{ "<leader>rr", "<cmd>VtrSendCommandToRunner! !!<cr>" },
+	},
+}
