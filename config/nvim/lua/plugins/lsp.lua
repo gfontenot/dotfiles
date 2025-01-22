300d83663fcc49e8a99380da3da3bbf9ace3f988
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-01-22 12:50:16 -0600

    nvim: Disable auto-formatting for Swift code
    This is unfortunate, but my work codebase is _wildly_ out of sync with
    the autoformatting preference and so using this feature results in more
    headaches than are worth it. In the future I might try to find a way to
    conditionally re-enable this for specific codebases, but I'll wait till
    I need that behavior to do that work.

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -213,7 +213,11 @@ return {
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
+				local disable_filetypes = {
+					c = true,
+					cpp = true,
+					swift = true,
+				}
 				local lsp_format_opt
 				if disable_filetypes[vim.bo[bufnr].filetype] then
 					lsp_format_opt = "never"
