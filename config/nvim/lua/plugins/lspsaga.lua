d3c2f1d811d1488a3a750472adaab6c0b3c95bfa
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-01-29 14:38:37 -0600

    nvim: Add lspsaga
    Still testing this out, but so far I really like the breadcrumbs feature
    (if nothing else)

---
+++ config/nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,12 @@
+return {
+	"nvimdev/lspsaga.nvim",
+	opts = {
+		lightbulb = {
+			enable = false,
+		},
+	},
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter", -- optional
+		"nvim-tree/nvim-web-devicons", -- optional
+	},
+}
