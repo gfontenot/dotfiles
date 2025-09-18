55e887522e897c8805c195ba2a0104f9655a7638
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-09-18 11:00:34 -0500

    nvim: Limit recent files to the cwd
    This was driving me crazy, especially when bouncing around between
    multiple projects quickly. It seems weird to me that this isn't the
    default but maybe I'm the weird one here.

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -9,7 +9,14 @@ return {
 			sections = {
 				{ section = "header" },
 				{ section = "keys", gap = 1, padding = 1 },
-				{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				{
+					icon = " ",
+					title = "Recent Files",
+					section = "recent_files",
+					cwd = true,
+					indent = 2,
+					padding = 1,
+				},
 				{ section = "startup" },
 			},
 		},
