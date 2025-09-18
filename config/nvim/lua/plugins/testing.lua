b7a63c765662f13f5db035671323ec5405b474c2
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-09-17 20:18:39 -0500

    nvim: Switch to neotest
    Neotest is a _super_ nice testing plugin that includes way more than
    vim-test ever did (like async running by default, pop up panes for test
    results, summary panes to show the tests in the project, etc) and _also_
    has an adaptor to be able to fall back to vim-test if there isn't a
    native plugin available. Pretty fantastic little library, I'm really
    enjoying it!

--- config/nvim/lua/plugins/testing.lua
+++ config/nvim/lua/plugins/testing.lua
@@ -1,13 +1,82 @@
 return {
-	"vim-test/vim-test",
-	keys = {
-		{ "<leader>tt", "<cmd>TestNearest<cr>" },
-		{ "<leader>tT", "<cmd>TestFile<cr>" },
-		{ "<leader>ta", "<cmd>TestSuite<cr>" },
-		{ "<leader>tl", "<cmd>TestLast<cr>" },
-		{ "<leader>tg", "<cmd>TestVisit<cr>" },
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+
+		"nvim-neotest/neotest-plenary",
+		"nvim-neotest/neotest-vim-test",
 	},
 	config = function()
-		vim.cmd('let test#strategy = "vtr"')
+		require("neotest").setup({
+			adapters = {
+				require("neotest-plenary"),
+				require("neotest-vim-test")({ ignore_filetypes = { "swift", "lua" } }),
+			},
+			output = {
+				enabled = true,
+				open_on_run = false,
+			},
+		})
 	end,
+	keys = {
+		{
+			"<Leader>tt",
+			function()
+				require("neotest").run.run()
+			end,
+			{ desc = "Run nearest test" },
+		},
+		{
+			"<Leader>tT",
+			function()
+				require("neotest").run.run(vim.fn.expand("%"))
+			end,
+			{ desc = "Run all tests in file" },
+		},
+		{
+			"<Leader>ta",
+			function()
+				require("neotest").run.run({ suite = true, extra_args = { target = true } })
+			end,
+			{ desc = "Run all tests in current target." },
+		},
+		{
+			"<Leader>tA",
+			function()
+				require("neotest").run.run({ suite = true })
+			end,
+			{ desc = "Run all tests in project" },
+		},
+		{
+			"<Leader>tr",
+			function()
+				require("neotest").run.run_last()
+			end,
+			{ desc = "Re-run the previous tests" },
+		},
+		{
+			"<Leader>ts",
+			function()
+				require("neotest").summary.toggle()
+			end,
+			{ silent = true, desc = "Test summary" },
+		},
+		{
+			"<Leader>to",
+			function()
+				require("neotest").output.open({ short = true, enter = true })
+			end,
+			{ silent = true, desc = "Open test output" },
+		},
+		{
+			"<Leader>tp",
+			function()
+				require("neotest").output_panel.toggle()
+			end,
+			{ silent = true, desc = "Toggle test output pane" },
+		},
+	},
 }
