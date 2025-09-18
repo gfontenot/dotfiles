c2d52be6d1ea143c913182f26ddee609598dd4c1
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-09-17 20:14:50 -0500

    nvim: Remove tmux related plugins
    I stopped using tmux so these aren't actually being used anymore. RIP i
    won't forget u

--- config/nvim/lua/plugins/vim-tmux-runner.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"christoomey/vim-tmux-runner",
-	keys = {
-		{ "<leader>ar", "<cmd>VtrAttachToPane<cr>" },
-		{ "<leader>fr", "<cmd>VtrFocusRunner<cr>" },
-		{ "<leader>rr", "<cmd>VtrSendCommandToRunner! !!<cr>" },
-	},
-}
