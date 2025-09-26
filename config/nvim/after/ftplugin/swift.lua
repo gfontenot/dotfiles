201fb2e60eb043a8f2e1e0bb42b6500bb02ee773
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-09-26 15:56:36 -0500

    nvim: Set up mini.splitjoin
    mini.splitjoin is a small utility for splitting and joining lines
    intelligently. It also supports local buffer-specific configs to modify
    the behavior for specific filetypes. So for example in Swift, I want to
    always add trailing commas for () and [] structures, and I want to pad
    {} structures with spaces when collapsing them down to a single line. I
    can configure this neatly inside the ftplugin.lua file (which I'm also
    moving from a flat vim file)

---
+++ config/nvim/after/ftplugin/swift.lua
@@ -0,0 +1,19 @@
+vim.opt_local.colorcolumn = "120"
+
+local gen_hook = MiniSplitjoin.gen_hook
+
+local parens_brackets = { brackets = { "%b()", "%b[]" } }
+local curly = { brackets = { "%b{}" } }
+
+-- Add trailing comma when splitting inside brackets
+local add_trailing_commas = gen_hook.add_trailing_separator(parens_brackets)
+-- Delete trailing comma when joining inside brackets
+local remove_trailing_commas = gen_hook.del_trailing_separator(parens_brackets)
+-- Pad curly brackets with single space after join
+local pad_curly = gen_hook.pad_brackets(curly)
+
+-- Create buffer-local config
+vim.b.minisplitjoin_config = {
+	split = { hooks_post = { add_trailing_commas } },
+	join = { hooks_post = { remove_trailing_commas, pad_curly } },
+}
---
+++ config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,7 @@
+return {
+	"nvim-mini/mini.nvim",
+
+	config = function()
+		require("mini.splitjoin").setup()
+	end,
+}
