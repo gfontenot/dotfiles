eaf53e53b386a76aea52a3042391291abca06696
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-10-03 11:40:21 -0500

    nvim: Add LSP symbols to the statusline

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -2,15 +2,28 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
+		"folke/trouble.nvim",
 	},
 	config = function()
+		local trouble = require("trouble")
+		local symbols = trouble.statusline({
+			mode = "lsp_document_symbols",
+			groups = {},
+			title = false,
+			filter = { range = true },
+			format = "{kind_icon}{symbol.name:Normal}",
+			-- The following line is needed to fix the background color
+			-- Set it to the lualine section you want to use
+			hl_group = "lualine_c_normal",
+		})
+
 		require("lualine").setup({
 			options = {
 				theme = "onedark",
 			},
 			sections = {
 				lualine_a = { { "filename", path = 1 } },
-				lualine_b = {},
+				lualine_b = { { symbols.get, cond = symbols.has } },
 				lualine_c = {},
 				lualine_x = {},
 				lualine_y = { "branch" },
