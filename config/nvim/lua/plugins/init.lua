3e7669783f0b1f61871d93d13c9ebabedfcfa369
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-10-03 09:50:33 -0500

    nvim: Remove unused dependencies
    I'm not writing any Haskell these days and I don't think vim-repeat
    actually does anything anymore.

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -6,12 +6,9 @@ return {
 
 	-- Languages
 	{ "sheerun/vim-polyglot" },
-	{ "pbrisbin/vim-syntax-shakespeare" },
 
 	-- Misc
 	{ "christoomey/vim-sort-motion" },
 	{ "christoomey/vim-system-copy" },
 	{ "dietsche/vim-lastplace" },
-	{ "pbrisbin/vim-mkdir" },
-	{ "tpope/vim-repeat" },
 }
