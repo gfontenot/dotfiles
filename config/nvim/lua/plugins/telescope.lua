0f8e872e8aeccf29d71db1d3a304e2ba61e01cd4
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-10-03 09:46:13 -0500

    nvim: Add descriptions to Telescope keybindings

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -17,16 +17,16 @@ return {
 		})
 
 		-- Telescope Remaps
-		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-		vim.keymap.set("n", "<C-b>", builtin.buffers, {})
-		vim.keymap.set("n", "<C-g>", builtin.git_status, {})
-		vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
-		vim.keymap.set("n", "<C-k>", builtin.grep_string, {})
+		vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
+		vim.keymap.set("n", "<C-b>", builtin.buffers, { desc = "Find open buffers" })
+		vim.keymap.set("n", "<C-g>", builtin.git_status, { desc = "Find dirty files" })
+		vim.keymap.set("n", "<C-f>", builtin.live_grep, { desc = "Live grep" })
+		vim.keymap.set("n", "<C-k>", builtin.grep_string, { desc = "Search for word under the cursor" })
 		vim.keymap.set(
 			"n",
 			"<C-e>",
 			":Telescope file_browser initial_mode=normal path=%:p:h select_buffer=true<CR>",
-			{}
+			{ desc = "Open file browser" }
 		)
 	end,
 }
