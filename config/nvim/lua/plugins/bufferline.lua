72515c75cf18f6b45f2d8a9166949359608b2b65
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-10-04 13:25:31 -0500

    nvim: Set up bufferline
    This is a fun plugin to present open buffers visually as tabs

---
+++ config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,15 @@
+return {
+	"akinsho/bufferline.nvim",
+	opts = {
+		options = {
+			offsets = {
+				{
+					filetype = "NvimTree",
+					text = "File Explorer",
+					text_align = "center",
+					separator = true,
+				},
+			},
+		},
+	},
+}
