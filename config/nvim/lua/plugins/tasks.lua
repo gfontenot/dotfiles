cfeb8c4544d310675557eb3e4af92da849fdfa36
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-10-04 11:03:18 -0500

    nvim: Add vstask keybindings

--- config/nvim/lua/plugins/tasks.lua
+++ config/nvim/lua/plugins/tasks.lua
@@ -8,6 +8,25 @@ return {
 	config = function()
 		require("vstask").setup({
 			require("telescope").load_extension("vstask"),
+			telescope_keys = {
+				vertical = "<C-v>",
+				split = "<CR>",
+				tab = "<C-t>",
+				current = "<C-=>",
+				background = "<C-b>",
+				watch_job = "<C-w>",
+				kill_job = "<C-d>",
+				run = "<C-r>",
+			},
 		})
 	end,
+	keys = {
+		{
+			"<Leader>cp",
+			function()
+				require("vstask").tasks()
+			end,
+			desc = "Open [C]ommand [P]icker",
+		},
+	},
 }
