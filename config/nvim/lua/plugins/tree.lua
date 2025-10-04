9ae0bdee36f0a73ae1c3e5b0b121dac7c32c497b
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-10-04 13:24:59 -0500

    nvim: Hide root label in nvim-tree

--- config/nvim/lua/plugins/tree.lua
+++ config/nvim/lua/plugins/tree.lua
@@ -1,14 +1,15 @@
 return {
 	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	lazy = false,
-	keys = {
-		{ "<Leader>e", "<cmd>NvimTreeFindFileToggle<cr>", desc = "Toggle Tree" },
-	},
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
-	config = function()
-		require("nvim-tree").setup({})
-	end,
+	lazy = false,
+	opts = {
+		renderer = {
+			root_folder_label = false,
+		},
+	},
+	keys = {
+		{ "<Leader>e", "<cmd>NvimTreeFindFileToggle<cr>", desc = "Toggle Tree" },
+	},
 }
