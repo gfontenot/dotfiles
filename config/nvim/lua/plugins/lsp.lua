30d5ae74c12936cfb3c63cb9f8b2ca895979d613
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-10-06 13:44:47 -0500

    nvim/conform: Remove keymap for formatting the current buffer
    I never used this and it's going to conflict with my new Telescope
    keymaps

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -239,16 +239,7 @@ return {
 		"stevearc/conform.nvim",
 		event = { "BufWritePre" },
 		cmd = { "ConformInfo" },
-		keys = {
-			{
-				"<leader>f",
-				function()
-					require("conform").format({ async = true, lsp_format = "fallback" })
-				end,
-				mode = "",
-				desc = "[F]ormat buffer",
-			},
-		},
+		keys = {},
 		opts = {
 			notify_on_error = false,
 			format_on_save = function(bufnr)
