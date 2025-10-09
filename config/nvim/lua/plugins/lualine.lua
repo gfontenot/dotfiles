ab3145faadb3f43f57b074042b4044487e5676d3
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2025-10-09 16:03:51 -0500

    nmiv/lualine: Remove symbols from status bar
    This is a _lot_ of noise, and the same information is now being
    presented to me in the editor from treesitter-context

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -5,25 +5,13 @@ return {
 		"folke/trouble.nvim",
 	},
 	config = function()
-		local trouble = require("trouble")
-		local symbols = trouble.statusline({
-			mode = "lsp_document_symbols",
-			groups = {},
-			title = false,
-			filter = { range = true },
-			format = "{kind_icon}{symbol.name:Normal}",
-			-- The following line is needed to fix the background color
-			-- Set it to the lualine section you want to use
-			hl_group = "lualine_c_normal",
-		})
-
 		require("lualine").setup({
 			options = {
 				theme = "gruvbox",
 			},
 			sections = {
 				lualine_a = { { "filename", path = 1 } },
-				lualine_b = { { symbols.get, cond = symbols.has } },
+				lualine_b = {},
 				lualine_c = {},
 				lualine_x = {},
 				lualine_y = { "branch" },
