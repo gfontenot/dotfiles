14aef7deba70e6dad9bb0bdbf22a59b7cfdeaf4d
Gordon Fontenot <fontenot@squareup.com>
2025-10-29 22:20:15 -0500

    nvim: Set my keymaps with which-key
    I just like this format a lot more

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -25,48 +25,62 @@ vim.opt.winborder = 'rounded'
 
 -- [[ MAPPINGS ]]
 
+local wk = require('which-key')
+
 -- Delete hjkl mappings since they don't make sense at all on my keyboard layout (Workman)
 -- I actually use the arrow keys for these operations (or I use better navigation modes)
-vim.keymap.set('n', 'h', '<Nop>')
-vim.keymap.set('n', 'j', '<Nop>')
-vim.keymap.set('n', 'k', '<Nop>')
-vim.keymap.set('n', 'l', '<Nop>')
+wk.add({
+  { 'h', '<Nop>', desc = 'Disabled' },
+  { 'j', '<Nop>', desc = 'Disabled' },
+  { 'k', '<Nop>', desc = 'Disabled' },
+  { 'l', '<Nop>', desc = 'Disabled' },
+})
 
 -- Split Management
+wk.add({
+  { '<C-\\>', '<C-w>s', desc = 'Create a new horizontal split' },
+  { '<C-->', '<C-w>v', desc = 'Create a new vertical split' },
+  {
+    '<A-Up>',
+    '<C-w><Up>',
+    desc = 'Move cursor to the split above the current one',
+  },
+  {
+    '<A-Down>',
+    '<C-w><Down>',
+    desc = 'Move cursor to the split below the current one',
+  },
+  {
+    '<A-b>', -- <A-Left>
+    '<C-w><Left>',
+    desc = 'Move cursor to the split to the left',
+  },
+  {
+    '<A-f>', -- <A-Right>
+    '<C-w><Right>',
+    desc = 'Move cursor to the split to the right',
+  },
+})
 
-vim.keymap.set('n', '<C-\\>', '<C-w>s', { desc = 'Create a new horizontal split' })
-vim.keymap.set('n', '<C-->', '<C-w>v', { desc = 'Create a new vertical split' })
+-- Misc
+wk.add({
+  { '<Leader><Leader>', '<C-^>', desc = 'Jump back to the previous file' },
 
--- <A-Right>
-vim.keymap.set('n', '<A-f>', '<C-w><Right>', { desc = 'Move cursor to the split to the right' })
--- <A-Left>
-vim.keymap.set('n', '<A-b>', '<C-w><Left>', { desc = 'Move cursor to the split to the left' })
-vim.keymap.set(
-  'n',
-  '<A-Down>',
-  '<C-w><Down>',
-  { desc = 'Move cursor to the split below the current one' }
-)
-vim.keymap.set(
-  'n',
-  '<A-Up>',
-  '<C-w><Up>',
-  { desc = 'Move cursor to the split above the current one' }
-)
-
--- Map <leader><leader> to switch to previous file
-vim.keymap.set('n', '<Leader><Leader>', '<C-^>', { desc = 'Jump back to the previous file' })
-
-vim.keymap.set('n', '<Esc>', function()
-  Util.close_floats()
-  if vim.bo.modifiable then
-    Util.clear_highlights()
-  else
-    if #vim.api.nvim_list_wins() > 1 then
-      return Util.feedkeys('<C-w>c')
-    end
-  end
-end, { desc = 'Close floats, clear highlights' })
+  {
+    '<Esc>',
+    function()
+      Util.close_floats()
+      if vim.bo.modifiable then
+        Util.clear_highlights()
+      else
+        if #vim.api.nvim_list_wins() > 1 then
+          return Util.feedkeys('<C-w>c')
+        end
+      end
+    end,
+    desc = 'Close floats, clear highlights',
+  },
+})
 
 vim.opt.gdefault = true
 
