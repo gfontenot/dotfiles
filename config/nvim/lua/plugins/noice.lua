1a2acb6c2ece217cbcdfb5ba46497847d47bb317
Gordon Fontenot <fontenot@squareup.com>
2025-11-03 10:24:15 -0600

    nvim/noice: Disable "hover" customization
    This was worse than the default and also broke borders on the hover
    window.

--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -13,6 +13,7 @@ return {
         ['vim.lsp.util.stylize_markdown'] = true,
         ['cmp.entry.get_documentation'] = true,
       },
+      hover = { enabled = false },
     },
     presets = {
       bottom_search = true, -- use a classic bottom cmdline for search
