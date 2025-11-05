d580d97711856435a633f07fbdd9c32cb8f21efd
Gordon Fontenot <fontenot@squareup.com>
2025-11-05 09:19:06 -0600

    nvim/snacks: Rework Snacks config
    I'm moving the more complicated config (keys, dashboard, picker) out
    into helper functions to hopefully make these easier to read. Not at all
    sure if it's successful.
    
    Also: this tweaks my dashboard a bit so that I have access to sessions,
    and also to update the keybindings for the various dashboard options.
    Maybe some other stuff too idk I kind of lost track of what I was doing
    in here.

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -1,3 +1,178 @@
+local function configure_keymaps(Snacks)
+  local wk = require('which-key')
+
+  wk.add({})
+
+  -- Picker
+  wk.add({
+    {
+      '<C-p>',
+      function()
+        Snacks.picker.files({ hidden = true })
+      end,
+      desc = 'Open file [P]icker',
+    },
+    {
+      '<Leader>pb',
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = '[P]ick open [B]uffer',
+    },
+    {
+      '<Leader>pg',
+      function()
+        Snacks.picker.git_status()
+      end,
+      desc = '[P]ick from [G]it status',
+    },
+    {
+      '<Leader>ps',
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = '[P]ick from live [S]earch',
+    },
+    {
+      '<C-k>',
+      function()
+        Snacks.picker.grep_word()
+      end,
+      desc = 'Search for the word under the cursor',
+    },
+    {
+      '<C-e>',
+      function()
+        Snacks.explorer()
+      end,
+      desc = 'Open file [E]xplorer',
+    },
+    {
+      '<Leader>pp',
+      function()
+        Snacks.picker()
+      end,
+      desc = '[P]ick from available [P]ickers',
+    },
+  })
+
+  -- Terminal
+  wk.add({
+    {
+      '<C-/>',
+      mode = { 'n', 't' },
+      function()
+        Snacks.terminal()
+      end,
+      desc = 'Toggle floating terminal',
+    },
+  })
+
+  -- Git
+  wk.add({
+    {
+      '<Leader>gg',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'Launch lazygit',
+    },
+    {
+      '<Leader>gb',
+      mode = { 'n', 'v' },
+      function()
+        Snacks.gitbrowse()
+      end,
+      desc = '[G]it [B]rowse',
+    },
+  })
+end
+
+-- Config for the dashboard
+local dashboard = {
+  enabled = true,
+  preset = {
+    keys = {
+      {
+        icon = ' ',
+        key = 'f',
+        desc = 'Find File',
+        action = ":lua Snacks.dashboard.pick('files')",
+      },
+      {
+        icon = ' ',
+        key = 's',
+        desc = 'Find Text',
+        action = ":lua Snacks.dashboard.pick('live_grep')",
+      },
+      {
+        icon = ' ',
+        key = 'r',
+        desc = 'Recent Files',
+        action = ":lua Snacks.dashboard.pick('oldfiles')",
+      },
+      {
+        icon = ' ',
+        key = 'S',
+        desc = 'Restore Session',
+        section = 'session',
+      },
+      {
+        icon = '󰒲 ',
+        key = 'L',
+        desc = 'Lazy',
+        action = ':Lazy',
+        enabled = package.loaded.lazy ~= nil,
+      },
+      { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+    },
+  },
+  sections = {
+    { section = 'header' },
+    { section = 'keys', gap = 1, padding = 1 },
+    {
+      icon = ' ',
+      title = 'Recent Files',
+      section = 'recent_files',
+      cwd = true,
+      indent = 2,
+      padding = 1,
+    },
+    { section = 'startup' },
+  },
+}
+
+-- Config for the picker
+local picker = {
+  enabled = true,
+  sources = {
+    explorer = {
+      win = {
+        list = {
+          keys = {
+            ['Y'] = { 'copy_path_to_clipboard', mode = { 'n', 'x' } },
+          },
+        },
+      },
+      actions = {
+        copy_path_to_clipboard = function(picker)
+          local files = {} ---@type string[]
+          if vim.fn.mode():find('^[vV]') then
+            picker.list:select()
+          end
+          for _, item in ipairs(picker:selected({ fallback = true })) do
+            table.insert(files, Snacks.picker.util.path(item))
+          end
+          picker.list:set_selected() -- clear selection
+          local value = table.concat(files, '\n')
+          vim.fn.setreg('+', value, 'l')
+          Snacks.notify.info('Yanked ' .. #files .. ' files')
+        end,
+      },
+    },
+  },
+}
+
 return {
   'folke/snacks.nvim',
   dependencies = {
@@ -9,148 +184,18 @@ return {
     local Snacks = require('snacks')
     Snacks.setup({
       bigfile = { enabled = true },
-      dashboard = {
-        enabled = true,
-        sections = {
-          { section = 'header' },
-          { section = 'keys', gap = 1, padding = 1 },
-          {
-            icon = ' ',
-            title = 'Recent Files',
-            section = 'recent_files',
-            cwd = true,
-            indent = 2,
-            padding = 1,
-          },
-          { section = 'startup' },
-        },
-      },
+      dashboard = dashboard,
       explorer = { enabled = true },
       indent = { enabled = true },
       input = { enabled = true },
       notifier = { enabled = true },
-      picker = {
-        enabled = true,
-        sources = {
-          explorer = {
-            win = {
-              list = {
-                keys = {
-                  ['Y'] = { 'copy_path_to_clipboard', mode = { 'n', 'x' } },
-                },
-              },
-            },
-            actions = {
-              copy_path_to_clipboard = function(picker)
-                local files = {} ---@type string[]
-                if vim.fn.mode():find('^[vV]') then
-                  picker.list:select()
-                end
-                for _, item in ipairs(picker:selected({ fallback = true })) do
-                  table.insert(files, Snacks.picker.util.path(item))
-                end
-                picker.list:set_selected() -- clear selection
-                local value = table.concat(files, '\n')
-                vim.fn.setreg('+', value, 'l')
-                Snacks.notify.info('Yanked ' .. #files .. ' files')
-              end,
-            },
-          },
-        },
-      },
+      picker = picker,
       quickfile = { enabled = true },
       scope = { enabled = true },
       scroll = { enabled = true },
       statuscolumn = { enabled = true },
       words = { enabled = true },
     })
-
-    local wk = require('which-key')
-
-    wk.add({})
-
-    -- Picker
-    wk.add({
-      {
-        '<C-p>',
-        function()
-          Snacks.picker.files({ hidden = true })
-        end,
-        desc = 'Open file [P]icker',
-      },
-      {
-        '<Leader>pb',
-        function()
-          Snacks.picker.buffers()
-        end,
-        desc = '[P]ick open [B]uffer',
-      },
-      {
-        '<Leader>pg',
-        function()
-          Snacks.picker.git_status()
-        end,
-        desc = '[P]ick from [G]it status',
-      },
-      {
-        '<Leader>ps',
-        function()
-          Snacks.picker.grep()
-        end,
-        desc = '[P]ick from live [S]earch',
-      },
-      {
-        '<C-k>',
-        function()
-          Snacks.picker.grep_word()
-        end,
-        desc = 'Search for the word under the cursor',
-      },
-      {
-        '<C-e>',
-        function()
-          Snacks.explorer()
-        end,
-        desc = 'Open file [E]xplorer',
-      },
-      {
-        '<Leader>pp',
-        function()
-          Snacks.picker()
-        end,
-        desc = '[P]ick from available [P]ickers',
-      },
-    })
-
-    -- Terminal
-    wk.add({
-      {
-        '<C-/>',
-        mode = { 'n', 't' },
-        function()
-          Snacks.terminal()
-        end,
-        desc = 'Toggle floating terminal',
-      },
-    })
-
-    -- Git
-    wk.add({
-      {
-        '<Leader>gg',
-        function()
-          Snacks.lazygit()
-        end,
-        desc = 'Launch lazygit',
-      },
-      {
-        '<Leader>gb',
-        mode = { 'n', 'v' },
-        function()
-          Snacks.gitbrowse()
-        end,
-        desc = '[G]it [B]rowse',
-      },
-    })
+    configure_keymaps(Snacks)
   end,
 }
