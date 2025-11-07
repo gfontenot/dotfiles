0dad174f27090eb0e8d1c1fca9508d35e0e1ce03
Gordon Fontenot <fontenot@squareup.com>
2025-11-07 11:59:47 -0600

    nvim/theme: Bring back window borders
    This was actually driving me a bit crazy. It's really nice to have these
    borderless windows in screenshots, but in practice it made it really
    hard for me to tell where one window started and the other ended.

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -3,6 +3,17 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    require('kanso').setup({
+      overrides = function(colors)
+        return {
+          -- Bring back window borders
+          WinSeparator = {
+            fg = colors.palette.mistBg3,
+            bg = colors.theme.ui.none,
+          },
+        }
+      end,
+    })
     vim.cmd([[colorscheme kanso-mist]])
   end,
 }
