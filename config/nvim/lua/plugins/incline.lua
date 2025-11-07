d0ca9cd8bec32a8d653872fd6c5a4a39920a5b2c
Gordon Fontenot <fontenot@squareup.com>
2025-11-07 12:01:54 -0600

    nvim: Add incline.nvim
    This adds a tiny floating label to windows that shows the filename by
    default. This works _really_ well with laststatus = 3 so that I can
    still see what files I have open without needing the full status bar for
    each window.

---
+++ config/nvim/lua/plugins/incline.lua
@@ -0,0 +1,13 @@
+return {
+  'b0o/incline.nvim',
+  event = 'VeryLazy',
+  opts = {
+    window = {
+      padding = 2,
+      margin = {
+        vertical = { top = 0, bottom = 0 },
+        horizontal = { left = 0, right = 2 },
+      },
+    },
+  },
+}
