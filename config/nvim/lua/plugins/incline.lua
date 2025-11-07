4accf11a8af905eef7b3dd0a72d76408f10fa3f1
Gordon Fontenot <fontenot@squareup.com>
2025-11-07 15:51:23 -0600

    nvim/incline: Show file diagnostics
    This adds the diagnostics for the current file to the floating title
    thing provided by Incline

--- config/nvim/lua/plugins/incline.lua
+++ config/nvim/lua/plugins/incline.lua
@@ -9,5 +9,36 @@ return {
         horizontal = { left = 0, right = 2 },
       },
     },
+    render = function(props)
+      local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+      local ft_icon, ft_color = require('mini.icons').get('file', filename)
+      local modified = vim.bo[props.buf].modified and 'bold,italic' or 'bold'
+
+      local function get_diagnostic_label()
+        local icons = { error = '', warn = '' }
+        local label = {}
+
+        for severity, icon in pairs(icons) do
+          local n = #vim.diagnostic.get(
+            props.buf,
+            { severity = vim.diagnostic.severity[string.upper(severity)] }
+          )
+          if n > 0 then
+            table.insert(label, { icon .. n .. ' ', group = 'DiagnosticSign' .. severity })
+          end
+        end
+        if #label > 0 then
+          table.insert(label, { '┊ ' })
+        end
+        return label
+      end
+
+      local buffer = {
+        { get_diagnostic_label() },
+        { (ft_icon or '') .. ' ', guifg = ft_color, guibg = 'none' },
+        { filename .. ' ', gui = modified },
+      }
+      return buffer
+    end,
   },
 }
