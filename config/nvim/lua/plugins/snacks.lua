3284cc677f6444d823802b55bfc59698132fff7b
Gordon Fontenot <fontenot@squareup.com>
2025-11-28 13:45:23 -0600

    nvim/sidekick: Add the ability to send results to sidekick from Snacks.picker

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -227,6 +227,21 @@ local function picker(snacks)
         },
       },
     },
+    actions = {
+      sidekick_send = function(...)
+        return require('sidekick.cli.picker.snacks').send(...)
+      end,
+    },
+    win = {
+      input = {
+        keys = {
+          ['<a-a>'] = {
+            'sidekick_send',
+            mode = { 'n', 'i' },
+          },
+        },
+      },
+    },
   }
 end
 
